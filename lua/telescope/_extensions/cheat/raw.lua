fc5dd09aaf15db22d55bed699bb5de46f7d0a844
Tami <tamibam@yahoo.com>
2021-01-21 23:31:42 +0300

    integrate sql.nvim & create interface to add cheats
    Co-authored-by: Conni2461 <Simon-Hauser@outlook.de>

---
+++ lua/telescope/_extensions/cheat/raw.lua
@@ -0,0 +1,98 @@
+local j = require'plenary.job'
+local scan = require'plenary.scandir'
+local sources = require
+local M = {}
+
+local function remove_dir(cwd)
+   local handle = vim.loop.fs_scandir(cwd)
+  while true do
+    local name, t = vim.loop.fs_scandir_next(handle)
+    if not name then break end
+
+    local new_cwd = cwd..'/'..name
+    if t == 'directory' then
+      local success = remove_dir(new_cwd)
+      if not success then return false end
+    else
+      local success = vim.loop.fs_unlink(new_cwd)
+      if not success then return false end
+    end
+  end
+
+  return vim.loop.fs_rmdir(cwd)
+end
+
+local clone = function(uri, path, cb)
+  return j:new({
+    command = "git",
+    args = { "clone", uri, path, "--depth=1" },
+    on_exit = function(_, code)
+      if code == 0 then
+        return cb()
+      else
+        error(string.format("%s couldn't be temporarily cloned", uri))
+      end
+    end
+  }):start()
+end
+
+local get_source_path = function(cb, source)
+  local path = "/tmp/cheat_sources/" .. source.name
+  if vim.loop.fs_stat(path) == nil then
+    local await = function() cb(path) end
+    clone(source.uri, path, await)
+  else
+    cb(path)
+  end
+end
+
+local get_file_paths = function(cb, source)
+  return get_source_path(function(path)
+    return scan.scan_dir_async(path .. source.root, {
+      search_pattern = source.pattern,
+      on_exit = cb,
+      depth = source.depth,
+      add_dirs = source.add_dirs
+    })
+  end, source)
+end
+
+local extract_data = function(cb, source)
+  return get_file_paths(vim.schedule_wrap(function(paths)
+    local data = {}
+    for i, path in ipairs(paths) do
+      local ns, keyword = source.get_ns_keyword(path)
+      data[i] = {
+        source = source.name,
+        ns = keyword and ns or source.ns,
+        keyword = keyword and keyword or ns,
+        ft = source.ft,
+        content = source.parse(path)
+      }
+    end
+    cb(data)
+  end), source)
+end
+
+M.__get = function(cb, lsources, data) -- conni <3
+  if table.getn(lsources) > 0 then
+    local source = table.remove(lsources, 1)
+    extract_data(function(res)
+      for _, v in ipairs(res) do
+        table.insert(data, v)
+      end
+      remove_dir("/tmp/cheat_sources/" .. source.name)
+      M.__get(cb, lsources, data)
+    end, source)
+  else
+    return cb(data)
+  end
+end
+
+M.get = function(cb)
+  local data = {}
+  local lsources = vim.deepcopy(M.sources)
+  M.__get(cb, lsources, data)
+end
+
+return M
---
+++ lua/telescope/_extensions/cheat/sources.lua
@@ -0,0 +1,50 @@
+local p = require'plenary.path'
+local M = {}
+
+local parse  = function(path)
+  local content = p.readlines(path)
+  if content[1]:find('---', 1, true) then
+    local minus_count = 0
+    while minus_count < 2 do
+      if content[1]:find('---', 1, true) then minus_count = minus_count + 1 end
+      table.remove(content, 1)
+    end
+  end
+  while content[1] == '' do
+    table.remove(content, 1)
+  end
+
+  return vim.fn.json_encode(content)
+end,
+
+M[1] =  {
+  name = "cheatsheets",
+  uri = "https://github.com/cheat/cheatsheets",
+  ns = "unix",
+  root = "",
+  pattern = "",
+  ft = "sh",
+  parse = parse,
+  get_ns_keyword = function(path)
+    local name = vim.split(path, "/")
+    local a, b = unpack(vim.split(name[#name], "-"))
+    return a, b
+  end
+}
+
+M[2] = {
+  name = "learnxinyminutes",
+  uri = "https://github.com/adambard/learnxinyminutes-docs",
+  root = "",
+  depth = 1,
+  pattern = ".*%.html%.markdown",
+  add_dirs = false,
+  ft = "markdown",
+  parse = parse,
+  get_ns_keyword = function(path)
+    return "lang", path:match('.*/([^./]+).*')
+  end
+}
+
+
+return M
