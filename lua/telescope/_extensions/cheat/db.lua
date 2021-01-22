9d61d4ae05898d5926ee52952c7fdc1a09ca8aa4
Tami <tamibam@yahoo.com>
2021-01-22 15:20:22 +0300

    WIP: cover deleted db problem
    with https://github.com/tami5/sql.nvim/commit/194fa2074137dd357f41d61248c1c1775acef183

--- lua/telescope/_extensions/cheat/db.lua
+++ lua/telescope/_extensions/cheat/db.lua
@@ -9,39 +9,40 @@ local db = sql.new((function()
   return d  .. "/telescope-cheat.db"
 end)())
 
-local state = (function()
-  local tbl = db:table("state")
-  local version
+local state = db:table("state")
 
-  tbl:schema {
+function state:__ensure_schema()
+  return self:schema {
     id = "number",
     version = "number",
     ensure = true
   }
+end
 
-  if not tbl.has_content then
-    tbl:insert{ id = 1, version = VERSION }
+function state:get_version()
+  self:__ensure_schema()
+  local version = self:get({ where = {id = 1}, keys = "version" })
+  return version[1] and version[1].version
+end
+
+function state:is_up_to_date()
+  if not self:get_version() then
+    self:insert{ id = 1, version = VERSION }
   end
+  return self:get_version() == VERSION
+end
 
-  function tbl:is_up_to_date()
-    version = self:get{where = {id = 1}, keys = "version"}
-    return version[1].version == VERSION
-  end
+function state:change_version()
+  return self:update{
+    where = { id = 1 },
+    values = { version = VERSION }
+  }
+end
 
-  function tbl:change_version()
-    return self:update{
-      where = { id = 1 },
-      values = { version = VERSION }
-    }
-  end
+local data = db:table("cheat")
 
-  return tbl
-end)()
-
-local data = (function()
-  local tbl = db:table("cheat")
-
-  tbl:schema {
+function data:__ensure_schema()
+  return self:schema {
     id = {"integer", "primary", "key"},
     source = "text",
     ns = "text",
@@ -50,44 +51,45 @@ local data = (function()
     ft = "text",
     ensure = true
   }
+end
 
-  function tbl:seed(cb)
-    if not self.has_content then
-      print("telesocpe-cheat.nvim: caching databases ........................ ")
-      return raw.get(function(rows)
-        self:insert(rows)
-        print("telesocpe-cheat.nvim: databases has been successfully cached.")
-        cb()
-      end)
-    else
-      cb()
-    end
+function data:seed(cb)
+  self:__ensure_schema()
+
+  print("telesocpe-cheat.nvim: caching databases ........................ ")
+  return raw.get(function(rows)
+    self:insert(rows)
+    print("telesocpe-cheat.nvim: databases has been successfully cached.")
+    cb()
+  end)
+end
+
+function data:recache(cb)
+  self:__ensure_schema()
+
+  print("telesocpe-cheat.nvim: recaching databases ...................... ")
+  return raw.get(function(rows)
+    print("telesocpe-cheat.nvim: databases has been successfully recached.")
+    self:replace(rows)
+    if cb then return cb() end
+  end)
+end
+
+function data:ensure(cb)
+  self:__ensure_schema()
+
+  local up_to_date = state:is_up_to_date()
+  local has_content = not self:empty()
+
+  if up_to_date and not has_content  then
+    return self:seed(cb)
+  elseif not up_to_date and has_content then
+    state:change_version()
+    return self:recache(cb)
+  else
+    cb()
   end
+end
 
-  function tbl:recache(cb)
-    if self.has_content then
-      print("telesocpe-cheat.nvim: recaching databases ...................... ")
-      return raw.get(function(rows)
-        print("telesocpe-cheat.nvim: databases has been successfully recached.")
-        self:replace(rows)
-        if cb then return cb() end
-      end)
-    end
-  end
-
-  function tbl:ensure(cb)
-    local up_to_date = state:is_up_to_date()
-    if up_to_date and not self.has_content  then
-      return self:seed(cb)
-    elseif not up_to_date and self.has_content then
-      state:change_version()
-      return self:recache(cb)
-    else
-      cb()
-    end
-  end
-
-  return tbl
-end)()
 
 return data
